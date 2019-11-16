b96df8789ca5d30dd4dab3e81a1bd4584caf8475
Nguyen Binh Nguyen <nguyen.nguyen@trustly.com>
2019-11-16 20:19:46 +0100

    Test Minio client to create bucket and put object

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # minio-demo
 A small demo shows how Minio works
+
+## What is MinIO
+MinIO is high performance Amazon S3 Compatible Cloud Storage.
+
+## Prerequisites
+* Docker
+* Java 11
+
+## How to run MinIO docker locally
+```
+docker run -p 9000:9000 -e "MINIO_ACCESS_KEY=minio" -e "MINIO_SECRET_KEY=minio123" minio/minio server /data
+```
+
+## How to test MinIO
+```
+gradle bootRun
+```
+
+After running the command above, the application will create a testing bucket if not existing 
+and upload testing_file.txt to that bucket. Go to to check the result:
+```
+http://127.0.0.1:9000/
+```
--- src/main/java/se/mikka/miniodemo/MinioDemoApplication.java
+++ src/main/java/se/mikka/miniodemo/MinioDemoApplication.java
@@ -1,13 +1,53 @@
 package se.mikka.miniodemo;
 
+import io.minio.MinioClient;
+import io.minio.errors.ErrorResponseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
 public class MinioDemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(MinioDemoApplication.class);
+
+	private static String accessKey = "minio";
+	private static String secretKey = "minio123";
+	private static String bucket = "testing";
+	private static String testingFile = "testing_file.txt";
+
 	public static void main(String[] args) {
 		SpringApplication.run(MinioDemoApplication.class, args);
+
+		try {
+			MinioClient minioClient = new MinioClient("http://127.0.0.1:9000", accessKey, secretKey);
+
+			boolean isExistBucket = minioClient.bucketExists(bucket);
+			if (isExistBucket) {
+				logger.info("Bucket {} is existing", bucket);
+			} else {
+				logger.info("Bucket {} is not existing. Creating it!", bucket);
+				minioClient.makeBucket(bucket);
+			}
+
+			// Creating file if not existing
+			try {
+				minioClient.statObject(bucket, testingFile);
+				logger.info("File {} is existing", testingFile);
+			} catch (ErrorResponseException e) {
+				logger.info("File {} is not existing. Creating it!", testingFile);
+
+				Resource resourceTestingFile = new ClassPathResource(testingFile);
+				minioClient.putObject(bucket, testingFile, resourceTestingFile.getInputStream(), resourceTestingFile.contentLength(),
+						null, null, "application/octet-stream");
+			}
+		} catch (Exception e) {
+			logger.error("Exception occurred with MinIO", e);
+			throw new RuntimeException("Exception occurred with MinIO");
+		}
 	}
 
 }
